809cf71de0ffd58c5d06ba3b803ff462cc4419af
mateo022 <mateoceballos022@gmail.com>
2024-08-26 09:21:41 -0500

    First steps using framework 7

---
+++ frontend/src/framework7.js
@@ -0,0 +1,6 @@
+import Framework7Vue from 'framework7-vue';
+import '../node_modules/framework7/framework7-bundle';
+import '../node_modules/framework7/framework7-bundle.css';
+
+
+Framework7.use(Framework7Vue);
