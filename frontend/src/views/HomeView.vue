376d7483f1f0dcd3156e41b72c2cb938c36d94a8
mateo022 <mateoceballos022@gmail.com>
2024-09-13 08:20:57 -0500

    Implementation some styles

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,16 @@
 <!-- JS -->
 <script setup>
 
-const messageTest = 'Teo say : Welcome'
+const messageTest = 'Teo make test with branch'
 </script>
 
 
 <!-- CSS -->
 <style>
+h1{
+  color: black;
+  font-size: 25px;
+}
 </style>
 
 <!-- HTML -->
